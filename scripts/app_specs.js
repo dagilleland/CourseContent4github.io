8ad21f39498dde55b22ca4c3fc68fbbce4b58c4c
dagilleland <dagilleland@shaw.ca>
2014-12-18 07:15:25 -0700

    Re-ramping up on Jasmine

--- scripts/app_specs.js
+++ scripts/app_specs.js
@@ -1,6 +1,22 @@
 ﻿'use strict';
+$(function () {
 
-// TODO: courseApp.appService
-//          should...
-//          Ensure app loads the app.json
-//          Report XHR errors to the console
+    // TODO: courseApp.appService
+    //          should...
+    //          Ensure app loads the app.json
+    //          Report XHR errors to the console
+    describe('appService', function () {
+        /* App.Services
+            contentServices
+                should get valid JSON content
+                should get HTML file content
+         */
+        it('should fail')
+
+        // Function implementations (hoisted above)
+        function justFail() {
+            var test = undefined;
+            expect(test).not.toBe(undefined);
+        }
+    });
+});
